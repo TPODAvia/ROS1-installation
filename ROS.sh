b074040187241eae1aaf55a8f2ae358f9e927cdb
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-02-06 19:18:18 +0300

    text update

--- ROS.sh
+++ ROS.sh
@@ -1,3 +1,10 @@
+# Note: don't push to the main branche dirrectry
+# Use 
+#   git checkout -b "new-branch" 
+# then 
+#   git checkout "main"
+#   git merge "new-branch"
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt install curl -y # if you haven't already installed curl
